2da497fd8b8787a70f7dee564419df1f75b95358
Tom Aston <126413031+TomAston1996@users.noreply.github.com>
2025-02-22 14:02:27 +0000

    chore: update type hints and docstrings

--- DelayFileParser.py
+++ DelayFileParser.py
@@ -1,3 +1,9 @@
+'''
+This is the main file for the application. It creates the main window and starts the application.
+
+Author: Tom Aston
+'''
+
 import tkinter as tk
 
 from src.parser.delay_file_reader import DelayFileReader
@@ -5,7 +11,10 @@ from src.parser.folder_scanner import FolderScanner
 from src.ui.app import App
 
 
-def main():
+def main() -> None:
+    '''
+    Application entry point
+    '''
     root = tk.Tk()
     App(root, DelayFileReader, FolderScanner)
     root.mainloop()
